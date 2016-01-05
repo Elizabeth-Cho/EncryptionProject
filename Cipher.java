1f22d0500601098cf6b3f5ecac8d457edf1a3f2a
Elizabeth Cho <SilverSapo@users.noreply.github.com>
2016-01-04 22:45:14 -0500

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -3,59 +3,48 @@ import java.util.HashMap;
 public class Cipher
 {
 	private Key key;
-	private Alphabet alpha;
+	private Alphabet alphabet;
 	private String cipher;
+	private String nKey;
+	private String nAlpha;
 	
 	public Cipher(String nKey, String nAlpha)
 	{
 		nKey = key.toString();
-		nAlpha = alpha.toStringAlpha();
-		cipher = nKey + nAlpha;
+		nAlpha = alphabet.toStringAlpha();
 	}
-	private String removeDuplicatesCipher() throws InvalidFormatException
+	
+	public void setCipher()
+	{
+		cipher = nKey + nAlpha;
+		this.removeDuplicatesCipher();
+	}
+	
+	private String removeDuplicatesCipher()
 	{
 		HashMap<Character, Integer> mapping = new HashMap<Character, Integer>();
-		int i = 0;
-		for(i = 0; i < cipher.length(); i++)
+		for (int i = 0; i < cipher.length(); i++)
 		{
-			mapping.put(Character.valueOf(Character.valueOf(cipher.charAt(i))), Integer.valueOf(0));
+			mapping.put(Character.valueOf(cipher.charAt(i)), Integer.valueOf(0));
 		}
-		for (i = 0; i < cipher.length(); i++)
+		for (int i = 0; i < cipher.length(); i++)
 		{
-			if (!mapping.containsKey(Character.valueOf(cipher.charAt(i))))
-			{
-				throw new InvalidFormatException("Characters not in alphabet");
-			}
-			else
-			{
-				int intConversion = mapping.get(Character.valueOf(cipher.charAt(i)));
-				intConversion++;
-				Integer newValue = Integer.valueOf(intConversion);
-				mapping.put(Character.valueOf(cipher.charAt(i)), newValue);
-			}
+			int intConversion = mapping.get(Character.valueOf(cipher.charAt(i)));
+			intConversion++;
+			Integer newValue = Integer.valueOf(intConversion);
+			mapping.put(Character.valueOf(cipher.charAt(i)), newValue);
 		}
-		for (i = 0; i < cipher.length(); i++)
+		for (int i = 0; i < cipher.length(); i++)
 		{
 			if (mapping.get(Character.valueOf(cipher.charAt(i))) > 1)
 			{
-				int intConversion = mapping.get(Character.valueOf(cipher.charAt(i)));
+				int intConversion = mapping.get(Character.valueOf(cipher.charAt(i))).intValue();
 				intConversion--;
 				Integer newValue = Integer.valueOf(intConversion);
 				mapping.put(Character.valueOf(cipher.charAt(i)), newValue);
-				cipher = cipher.substring(0, i) + cipher.substring(i+1);
+				cipher = cipher.substring(0, i) + cipher.substring(i + 1);
 			}
 		}
-		
 		return cipher;
-	}
-	
-	public String encrypt()
-	{
-		
-	}
-	
-	public String decrypt()
-	{
-		
 	}
 }

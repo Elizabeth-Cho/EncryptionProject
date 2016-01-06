7f68e079997eb7aa487cb0b03e5f87a017dbc414
Elizabeth Cho <SilverSapo@users.noreply.github.com>
2016-01-06 01:49:30 -0500

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -7,29 +7,29 @@ public class Alphabet
 	public Alphabet(String nAlphabet)
 	{
 		this.alphabet = nAlphabet;
-		this.removeDuplicatesAlphabet();
+		this.removeDuplicatesAlpha();
+	}
+		
+	public String toStringAlpha()
+	{
+		return this.alphabet.toString();
 	}
 	
-	private void removeDuplicatesAlphabet()
+	private void removeDuplicatesAlpha()
 	{
 		HashMap<Character, Integer> mapping = new HashMap<Character, Integer>();
 		int i = 0;
 		while (i < this.alphabet.length())
 		{
-			if(!mapping.containsKey(Character.valueOf(alphabet.charAt(i))))
-			{
-				mapping.put(Character.valueOf(this.alphabet.charAt(i)), Integer.valueOf(0));
-				i++;
-			}
-			else
-			{
-				this.alphabet = this.alphabet.substring(0, i) + this.alphabet.substring(i + 1);
-			}
+			 if (!mapping.containsKey(Character.valueOf(this.alphabet.charAt(i))))
+			 {
+				 mapping.put(Character.valueOf(this.alphabet.charAt(i)), Integer.valueOf(0));
+				 i++;
+			 }
+			 else
+			 {
+				 this.alphabet = this.alphabet.substring(0, i) + this.alphabet.substring(i + 1);
+			 }
 		}
-	}
-	
-	public String toStringAlpha()
-	{
-		return this.alphabet.toString();
 	}
 }
